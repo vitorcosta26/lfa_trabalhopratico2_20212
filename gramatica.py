615e024bc702d7052027af55484c6a3f3e49f6ff
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-10-28 17:37:42 -0300

    Ler arquivo e salvar em memória
    Método para ler um arquivo e salvar a gramática em memória.

---
+++ gramatica.py
@@ -0,0 +1,30 @@
+class Gramatica:
+    
+    # Construtor
+    def __init__(self, arquivo):
+        self.gramatica, s_i, t, n_t = self.ler_gramatica(arquivo)
+        self.simbolo_inicial = s_i
+        self.terminais = t
+        self.nao_terminais = n_t
+
+    # Ler arquivo e salvar a gramática em memória
+    @staticmethod
+    def ler_gramatica(arquivo):
+
+        with open(arquivo + '.txt', 'r') as gramatica:
+            gramatica = gramatica.read().splitlines()
+
+        simbolo_inicial = gramatica[0][0]
+        terminais = []
+        nao_terminais = []
+        for linha in gramatica:
+            regra, producao = linha.split("=>")
+            producao = producao.split("|")
+
+            for simbolo in producao:
+                if str.islower(simbolo):
+                    terminais.append([regra, simbolo])
+                if not str.islower(simbolo):
+                    nao_terminais.append([regra, simbolo])
+
+        return gramatica, simbolo_inicial, terminais, nao_terminais
