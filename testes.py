7ad15e6257954a50fd24f2431efca866d8000763
vitorcosta26 <60762180+vitorcosta26@users.noreply.github.com>
2021-11-22 10:49:59 -0300

    Update testes.py

--- testes.py
+++ testes.py
@@ -1,30 +1,35 @@
 from gramatica import Gramatica
 
 gramatica0 = Gramatica('gramatica0')
+gramatica0.imprimir_gramatica()
 #Pertence
 gramatica0.testr_palavra_cyk('aabb')
 #Não pertence
 gramatica0.testr_palavra_cyk('aba')
 
 gramatica1 = Gramatica('gramatica1')
+gramatica1.imprimir_gramatica()
 #Pertence
 gramatica1.testr_palavra_cyk('ab')
 #Não pertence
 gramatica1.testr_palavra_cyk('aba')
 
 gramatica2 = Gramatica('gramatica2')
+gramatica2.imprimir_gramatica()
 #Pertence
 gramatica2.testr_palavra_cyk('bbaa')
 #Não pertence
 gramatica2.testr_palavra_cyk('aba')
 
 gramatica3 = Gramatica('gramatica3')
+gramatica3.imprimir_gramatica()
 #Pertence
 gramatica3.testr_palavra_cyk('baba')
 #Não pertence
 gramatica3.testr_palavra_cyk('aba')
 
 gramatica4 = Gramatica('gramatica4')
+gramatica4.imprimir_gramatica()
 #Pertence
 gramatica4.testr_palavra_cyk('bab')
 #Não pertence
